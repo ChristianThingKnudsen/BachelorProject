a656bdd02b8c612a9c11869e08fad6b57e1dff7b
Christian Thing Knudsen <ct-1000@hotmail.com>
2019-09-20 12:15:52 +0200

    First draft SCUI

---
+++ build-BachelorProjekt-Desktop_Qt_5_13_0_MSVC2017_64bit-Fejlretning/BachelorProjekt_autogen/include/ui_mainwindow.h
@@ -0,0 +1,204 @@
+/********************************************************************************
+** Form generated from reading UI file 'mainwindow.ui'
+**
+** Created by: Qt User Interface Compiler version 5.13.0
+**
+** WARNING! All changes made in this file will be lost when recompiling UI file!
+********************************************************************************/
+
+#ifndef UI_MAINWINDOW_H
+#define UI_MAINWINDOW_H
+
+#include <QtCore/QVariant>
+#include <QtWidgets/QAction>
+#include <QtWidgets/QApplication>
+#include <QtWidgets/QCheckBox>
+#include <QtWidgets/QComboBox>
+#include <QtWidgets/QHBoxLayout>
+#include <QtWidgets/QLabel>
+#include <QtWidgets/QMainWindow>
+#include <QtWidgets/QMenu>
+#include <QtWidgets/QMenuBar>
+#include <QtWidgets/QPushButton>
+#include <QtWidgets/QStatusBar>
+#include <QtWidgets/QTabWidget>
+#include <QtWidgets/QVBoxLayout>
+#include <QtWidgets/QWidget>
+
+QT_BEGIN_NAMESPACE
+
+class Ui_MainWindow
+{
+public:
+    QWidget *centralwidget;
+    QTabWidget *tabWidget;
+    QWidget *SCUI;
+    QWidget *verticalLayoutWidget;
+    QVBoxLayout *verticalLayout;
+    QHBoxLayout *horizontalLayoutRegion;
+    QLabel *labelRegion;
+    QComboBox *comboBox;
+    QHBoxLayout *horizontalLayoutLoadCT;
+    QPushButton *btnLoadCT;
+    QCheckBox *cbCT;
+    QHBoxLayout *horizontalLayoutLoadCBCT;
+    QPushButton *btnLoadCBCT;
+    QCheckBox *cbCBCT;
+    QHBoxLayout *horizontalLayoutSC;
+    QPushButton *btnScatterCorrect;
+    QWidget *verticalLayoutWidget_2;
+    QVBoxLayout *verticalLayoutImage;
+    QLabel *label_ID;
+    QLabel *labelImage;
+    QCheckBox *cbShowOrgans;
+    QWidget *Advanced;
+    QMenuBar *menubar;
+    QMenu *menuSCUI;
+    QStatusBar *statusbar;
+
+    void setupUi(QMainWindow *MainWindow)
+    {
+        if (MainWindow->objectName().isEmpty())
+            MainWindow->setObjectName(QString::fromUtf8("MainWindow"));
+        MainWindow->resize(932, 555);
+        centralwidget = new QWidget(MainWindow);
+        centralwidget->setObjectName(QString::fromUtf8("centralwidget"));
+        tabWidget = new QTabWidget(centralwidget);
+        tabWidget->setObjectName(QString::fromUtf8("tabWidget"));
+        tabWidget->setGeometry(QRect(0, 0, 801, 511));
+        SCUI = new QWidget();
+        SCUI->setObjectName(QString::fromUtf8("SCUI"));
+        verticalLayoutWidget = new QWidget(SCUI);
+        verticalLayoutWidget->setObjectName(QString::fromUtf8("verticalLayoutWidget"));
+        verticalLayoutWidget->setGeometry(QRect(0, 0, 231, 471));
+        verticalLayout = new QVBoxLayout(verticalLayoutWidget);
+        verticalLayout->setObjectName(QString::fromUtf8("verticalLayout"));
+        verticalLayout->setContentsMargins(0, 0, 0, 0);
+        horizontalLayoutRegion = new QHBoxLayout();
+        horizontalLayoutRegion->setObjectName(QString::fromUtf8("horizontalLayoutRegion"));
+        labelRegion = new QLabel(verticalLayoutWidget);
+        labelRegion->setObjectName(QString::fromUtf8("labelRegion"));
+
+        horizontalLayoutRegion->addWidget(labelRegion);
+
+        comboBox = new QComboBox(verticalLayoutWidget);
+        comboBox->setObjectName(QString::fromUtf8("comboBox"));
+
+        horizontalLayoutRegion->addWidget(comboBox);
+
+
+        verticalLayout->addLayout(horizontalLayoutRegion);
+
+        horizontalLayoutLoadCT = new QHBoxLayout();
+        horizontalLayoutLoadCT->setObjectName(QString::fromUtf8("horizontalLayoutLoadCT"));
+        btnLoadCT = new QPushButton(verticalLayoutWidget);
+        btnLoadCT->setObjectName(QString::fromUtf8("btnLoadCT"));
+
+        horizontalLayoutLoadCT->addWidget(btnLoadCT);
+
+        cbCT = new QCheckBox(verticalLayoutWidget);
+        cbCT->setObjectName(QString::fromUtf8("cbCT"));
+        cbCT->setAutoFillBackground(false);
+        cbCT->setChecked(false);
+
+        horizontalLayoutLoadCT->addWidget(cbCT);
+
+
+        verticalLayout->addLayout(horizontalLayoutLoadCT);
+
+        horizontalLayoutLoadCBCT = new QHBoxLayout();
+        horizontalLayoutLoadCBCT->setObjectName(QString::fromUtf8("horizontalLayoutLoadCBCT"));
+        btnLoadCBCT = new QPushButton(verticalLayoutWidget);
+        btnLoadCBCT->setObjectName(QString::fromUtf8("btnLoadCBCT"));
+
+        horizontalLayoutLoadCBCT->addWidget(btnLoadCBCT);
+
+        cbCBCT = new QCheckBox(verticalLayoutWidget);
+        cbCBCT->setObjectName(QString::fromUtf8("cbCBCT"));
+
+        horizontalLayoutLoadCBCT->addWidget(cbCBCT);
+
+
+        verticalLayout->addLayout(horizontalLayoutLoadCBCT);
+
+        horizontalLayoutSC = new QHBoxLayout();
+        horizontalLayoutSC->setObjectName(QString::fromUtf8("horizontalLayoutSC"));
+        btnScatterCorrect = new QPushButton(verticalLayoutWidget);
+        btnScatterCorrect->setObjectName(QString::fromUtf8("btnScatterCorrect"));
+
+        horizontalLayoutSC->addWidget(btnScatterCorrect);
+
+
+        verticalLayout->addLayout(horizontalLayoutSC);
+
+        verticalLayoutWidget_2 = new QWidget(SCUI);
+        verticalLayoutWidget_2->setObjectName(QString::fromUtf8("verticalLayoutWidget_2"));
+        verticalLayoutWidget_2->setGeometry(QRect(230, 0, 451, 471));
+        verticalLayoutImage = new QVBoxLayout(verticalLayoutWidget_2);
+        verticalLayoutImage->setObjectName(QString::fromUtf8("verticalLayoutImage"));
+        verticalLayoutImage->setContentsMargins(0, 0, 0, 0);
+        label_ID = new QLabel(verticalLayoutWidget_2);
+        label_ID->setObjectName(QString::fromUtf8("label_ID"));
+
+        verticalLayoutImage->addWidget(label_ID);
+
+        labelImage = new QLabel(verticalLayoutWidget_2);
+        labelImage->setObjectName(QString::fromUtf8("labelImage"));
+        labelImage->setMargin(10);
+
+        verticalLayoutImage->addWidget(labelImage);
+
+        cbShowOrgans = new QCheckBox(SCUI);
+        cbShowOrgans->setObjectName(QString::fromUtf8("cbShowOrgans"));
+        cbShowOrgans->setGeometry(QRect(700, 240, 91, 20));
+        tabWidget->addTab(SCUI, QString());
+        Advanced = new QWidget();
+        Advanced->setObjectName(QString::fromUtf8("Advanced"));
+        tabWidget->addTab(Advanced, QString());
+        MainWindow->setCentralWidget(centralwidget);
+        menubar = new QMenuBar(MainWindow);
+        menubar->setObjectName(QString::fromUtf8("menubar"));
+        menubar->setGeometry(QRect(0, 0, 932, 21));
+        menuSCUI = new QMenu(menubar);
+        menuSCUI->setObjectName(QString::fromUtf8("menuSCUI"));
+        MainWindow->setMenuBar(menubar);
+        statusbar = new QStatusBar(MainWindow);
+        statusbar->setObjectName(QString::fromUtf8("statusbar"));
+        MainWindow->setStatusBar(statusbar);
+
+        menubar->addAction(menuSCUI->menuAction());
+
+        retranslateUi(MainWindow);
+
+        tabWidget->setCurrentIndex(0);
+
+
+        QMetaObject::connectSlotsByName(MainWindow);
+    } // setupUi
+
+    void retranslateUi(QMainWindow *MainWindow)
+    {
+        MainWindow->setWindowTitle(QCoreApplication::translate("MainWindow", "MainWindow", nullptr));
+        labelRegion->setText(QCoreApplication::translate("MainWindow", "Region:", nullptr));
+        btnLoadCT->setText(QCoreApplication::translate("MainWindow", "Load CT", nullptr));
+        cbCT->setText(QString());
+        btnLoadCBCT->setText(QCoreApplication::translate("MainWindow", "Load CBCT", nullptr));
+        cbCBCT->setText(QString());
+        btnScatterCorrect->setText(QCoreApplication::translate("MainWindow", "Scatter Correct", nullptr));
+        label_ID->setText(QCoreApplication::translate("MainWindow", "ID:xxxxxx-xxxx", nullptr));
+        labelImage->setText(QCoreApplication::translate("MainWindow", "This is going to be an image later", nullptr));
+        cbShowOrgans->setText(QCoreApplication::translate("MainWindow", "Show Organs", nullptr));
+        tabWidget->setTabText(tabWidget->indexOf(SCUI), QCoreApplication::translate("MainWindow", "SCUI", nullptr));
+        tabWidget->setTabText(tabWidget->indexOf(Advanced), QCoreApplication::translate("MainWindow", "Advanced", nullptr));
+        menuSCUI->setTitle(QCoreApplication::translate("MainWindow", "SCUI", nullptr));
+    } // retranslateUi
+
+};
+
+namespace Ui {
+    class MainWindow: public Ui_MainWindow {};
+} // namespace Ui
+
+QT_END_NAMESPACE
+
+#endif // UI_MAINWINDOW_H
---
+++ build-BachelorProjekt-Desktop_Qt_5_13_0_MSVC2017_64bit-Fejlretning/cmake_install.cmake
@@ -0,0 +1,44 @@
+# Install script for directory: C:/Users/ct-10/OneDrive - Aarhus universitet/7 Semester ST/Bachelor/UI_Kode/BachelorProject/BachelorProjekt
+
+# Set the install prefix
+if(NOT DEFINED CMAKE_INSTALL_PREFIX)
+  set(CMAKE_INSTALL_PREFIX "C:/Program Files (x86)/BachelorProjekt")
+endif()
+string(REGEX REPLACE "/$" "" CMAKE_INSTALL_PREFIX "${CMAKE_INSTALL_PREFIX}")
+
+# Set the install configuration name.
+if(NOT DEFINED CMAKE_INSTALL_CONFIG_NAME)
+  if(BUILD_TYPE)
+    string(REGEX REPLACE "^[^A-Za-z0-9_]+" ""
+           CMAKE_INSTALL_CONFIG_NAME "${BUILD_TYPE}")
+  else()
+    set(CMAKE_INSTALL_CONFIG_NAME "Debug")
+  endif()
+  message(STATUS "Install configuration: \"${CMAKE_INSTALL_CONFIG_NAME}\"")
+endif()
+
+# Set the component getting installed.
+if(NOT CMAKE_INSTALL_COMPONENT)
+  if(COMPONENT)
+    message(STATUS "Install component: \"${COMPONENT}\"")
+    set(CMAKE_INSTALL_COMPONENT "${COMPONENT}")
+  else()
+    set(CMAKE_INSTALL_COMPONENT)
+  endif()
+endif()
+
+# Is this installation the result of a crosscompile?
+if(NOT DEFINED CMAKE_CROSSCOMPILING)
+  set(CMAKE_CROSSCOMPILING "FALSE")
+endif()
+
+if(CMAKE_INSTALL_COMPONENT)
+  set(CMAKE_INSTALL_MANIFEST "install_manifest_${CMAKE_INSTALL_COMPONENT}.txt")
+else()
+  set(CMAKE_INSTALL_MANIFEST "install_manifest.txt")
+endif()
+
+string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
+       "${CMAKE_INSTALL_MANIFEST_FILES}")
+file(WRITE "C:/Users/ct-10/OneDrive - Aarhus universitet/7 Semester ST/Bachelor/UI_Kode/BachelorProject/build-BachelorProjekt-Desktop_Qt_5_13_0_MSVC2017_64bit-Fejlretning/${CMAKE_INSTALL_MANIFEST}"
+     "${CMAKE_INSTALL_MANIFEST_CONTENT}")
