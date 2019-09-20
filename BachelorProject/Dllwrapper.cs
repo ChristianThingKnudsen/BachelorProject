a327dd35fedc6673d2ebe7bf77ca9b045975428f
Christian Thing Knudsen <ct-1000@hotmail.com>
2019-09-20 08:30:12 +0200

    Last WPF changes. Problems with calling QT functions

---
+++ BachelorProject/Dllwrapper.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BachelorProject
+{
+    public static class Dllwrapper
+    {
+        //[DllImport("CbctReconDlld.dll")]
+        
+       
+            [DllImport("CbctReconDlld.dll")]
+            public static extern int BuildRTKCommandFilter();// From DlgExternalCommand.h
+        
+        
+
+
+        //public static extern Boolean msgBox(IntPtr h, string m, string c, int t);
+        //public static extern IntPtr functionname([MarshalAs(UnmanagedType.FunctionPtr)] GuiUpdateDelegate gu, [MarshalAs(UnmanagedType.FunctionPtr)] LogDelegate ld);
+
+    }
+}
--- BachelorProject/MainWindow.xaml.cs
+++ BachelorProject/MainWindow.xaml.cs
@@ -20,9 +20,34 @@ namespace BachelorProject
     /// </summary>
     public partial class MainWindow : Window
     {
+        
         public MainWindow()
         {
             InitializeComponent();
+            // Setting collors
+            BrushConverter bc = new BrushConverter();         
+            mainWindow.Background = (Brush)bc.ConvertFrom("#0F4571");
+            ButtonLoadCT.Background = (Brush)bc.ConvertFrom("#1367AB");
+            ButtonLoadCBCT.Background = (Brush)bc.ConvertFrom("#1367AB");
+            
+
+        }
+        private void loadCT(object sender, RoutedEventArgs e)
+        {
+            int test = Dllwrapper.BuildRTKCommandFilter();
+            MessageBox.Show("Number: "+ test);
+        }
+        private void loadCBCT(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("This Button was Clicked");
+        }
+        private void checkCheckBoxCT(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("This Button was Clicked");
+        }
+        private void goToInfo(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("This Button was Clicked");
         }
     }
 }
