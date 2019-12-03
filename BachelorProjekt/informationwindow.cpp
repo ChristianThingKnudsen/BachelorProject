ce00f9bc07bec90ebdf4faea258def3dc849f28d
Christian Thing Knudsen <ct-1000@hotmail.com>
2019-12-03 23:52:20 +0100

    Information userinterface updated

--- BachelorProjekt/informationwindow.cpp
+++ BachelorProjekt/informationwindow.cpp
@@ -6,7 +6,12 @@ InformationWindow::InformationWindow(QWidget *parent) :
     ui(new Ui::InformationWindow)
 {
     ui->setupUi(this);
-
+    //Setting title and icon
+    this->setWindowTitle("Information Window");
+    //this->setWindowIcon(QIcon(parent->Root+QString("/pictures/logo.png")));
+    QPixmap pix("/Users/ct-10/OneDrive - Aarhus universitet/7 Semester ST/Bachelor/UI_Kode/BachelorProject/pictures/scui.png");
+    pix.scaled(660,360);
+    ui->labelImage->setPixmap(pix);
 }
 
 InformationWindow::~InformationWindow()
