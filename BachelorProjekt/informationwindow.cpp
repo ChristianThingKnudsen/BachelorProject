b0c58e2b1af5e7a841d69fb4426599e492a94376
Christian Thing Knudsen <ct-1000@hotmail.com>
2019-12-10 11:35:37 +0100

    Information window updated to final version

--- BachelorProjekt/informationwindow.cpp
+++ BachelorProjekt/informationwindow.cpp
@@ -13,10 +13,11 @@ InformationWindow::InformationWindow(QWidget *parent) :
     this->setWindowTitle("Information Window");
     this->setWindowIcon(QIcon(scui->Root+QString("/pictures/logo.png")));
     QPixmap pix(scui->Root+QString("/pictures/scui.png"));
-    int w = ui->labelImage->width();
-    int h = ui->labelImage->height();
-    ui->labelImage->setPixmap(pix.scaled(w,h,Qt::KeepAspectRatio));
-    ui->labelImage->setAlignment(Qt::AlignTop);
+    int w = ui->labelImage->width(); // Width
+    int h = ui->labelImage->height(); // Height
+    ui->labelImage->setPixmap(pix.scaled(w,h,Qt::KeepAspectRatio)); // Scaling
+    ui->labelImage->setAlignment(Qt::AlignTop); // Align top
+    setWindowFlags(windowFlags() & ~Qt::WindowContextHelpButtonHint);// removes question mark from title bar
 }
 
 InformationWindow::~InformationWindow()
